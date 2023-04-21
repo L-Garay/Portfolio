0432e489e423addc64b83602eb89eb555f64c95d
L-Garay <garay.logan@gmail.com>
2023-04-21 09:18:30 -0600

    fix useWindowDimensions to properly update

--- src/utils/hooks/useWindowDimensions.tsx
+++ src/utils/hooks/useWindowDimensions.tsx
@@ -4,17 +4,21 @@ export function useWindowDimensions() {
   const [windowWidth, setWindowWidth] = React.useState(0);
   const [windowHeight, setWindowHeight] = React.useState(0);
 
-  const isClient = typeof window !== 'undefined';
+  function handleResize(e: Event) {
+    const target = e.target as Window;
+    setWindowWidth(target.innerWidth);
+    setWindowHeight(target.innerHeight);
+  }
 
   React.useEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth);
-      setWindowHeight(window.innerHeight);
-    }
+    setWindowHeight(window.innerHeight);
+    setWindowWidth(window.innerWidth);
+  }, []);
 
+  React.useEffect(() => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
-  }, [isClient]);
+  }, []);
 
   return { windowWidth, windowHeight };
 }
