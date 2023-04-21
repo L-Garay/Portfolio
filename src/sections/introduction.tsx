e42468d70b1e9f16f1d5a68a02461e805f2b5da3
L-Garay <garay.logan@gmail.com>
2023-04-21 09:21:04 -0600

    basic intro section setup

--- src/sections/introduction.tsx
+++ src/sections/introduction.tsx
@@ -2,6 +2,25 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { Section, SectionContent } from '../components/shared/sections';
 import { useWindowDimensions } from '../utils/hooks/useWindowDimensions';
+import theme from '../styles/theme';
+import { Heading2, Paragraph } from '../components/shared';
+
+const IntroductionContent = styled.div`
+  text-align: start;
+`;
+
+const Heading = styled.h1`
+  margin: 0;
+  font-size: 5rem;
+  color: ${theme.colors.BLUE_1};
+  font-family: ${theme.fonts.robotoMono};
+`;
+
+const IntroParagraph = styled(Paragraph)``;
+
+const IntroHeading2 = styled(Heading2)`
+  color: ${theme.colors.ORANGE_1};
+`;
 
 const Introduction = () => {
   const { windowWidth, windowHeight } = useWindowDimensions();
@@ -9,7 +28,17 @@ const Introduction = () => {
   return (
     <Section id="introduction" height={windowHeight}>
       <SectionContent width={windowWidth - 200}>
-        <h1 style={{ margin: 0 }}>introduction section</h1>
+        <IntroductionContent>
+          <IntroParagraph>My name is, </IntroParagraph>
+          <Heading>Logan Garay</Heading>
+          <IntroHeading2>I am a Software Developer</IntroHeading2>
+          <IntroParagraph>
+            With a passion for creating simple and elogant digital experiences.
+            <br />
+            Looking to make tangible impact through code to better the world
+            around us.
+          </IntroParagraph>
+        </IntroductionContent>
       </SectionContent>
     </Section>
   );
