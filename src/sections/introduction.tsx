6e18afba394be0ea07f1bf4e7d931b34aac9e3d9
L-Garay <garay.logan@gmail.com>
2023-05-08 11:34:16 -0600

    update intro section to transform content after animation and update section spacing

--- src/sections/introduction.tsx
+++ src/sections/introduction.tsx
@@ -4,29 +4,56 @@ import { Section, SectionContent } from '../components/sections';
 import theme from '../styles/theme';
 import { Heading2, Paragraph } from '../components/shared';
 import { useDeviceContext } from '../contexts/deviceContext';
+import { useIntroContext } from '../contexts/introContext';
 import SCREEN_SIZES from '../constants/screenSizes';
+
+type IntroductionProps = {
+  hasSeenIntro: boolean;
+};
 
 const IntroductionContent = styled.div`
   text-align: start;
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
-const Heading = styled.h1`
+const Heading = styled.h1<IntroductionProps>`
   margin: 0;
   font-size: clamp(2.5rem, 4vw, 4.5rem);
   color: ${theme.colors.BLUE_1};
   font-family: ${theme.fonts.robotoMono};
+  opacity: ${({ hasSeenIntro }) => (hasSeenIntro ? 1 : 0)};
+  transform: ${({ hasSeenIntro }) =>
+    hasSeenIntro ? 'translateY(0)' : 'translateY(50px)'};
+  transition: all 0.5s linear 1s;
 `;
 
-const IntroParagraph = styled(Paragraph)`
-  font-size: max(0.8rem, 1.1vw);
+const IntroParagraph = styled(Paragraph)<IntroductionProps>`
+  font-size: max(1rem, 1.1vw);
+  opacity: ${({ hasSeenIntro }) => (hasSeenIntro ? 1 : 0)};
+  transform: ${({ hasSeenIntro }) =>
+    hasSeenIntro ? 'translateY(0)' : 'translateY(50px)'};
+  transition: all 0.5s linear 0.75s;
+
+  &.introduction2 {
+    transition: all 0.5s linear 1.5s;
+    font-size: max(0.9rem, 1.1vw);
+  }
 `;
 
-const IntroHeading2 = styled(Heading2)`
+const IntroHeading2 = styled(Heading2)<IntroductionProps>`
   font-size: clamp(1.75rem, 3vw, 3.5rem);
   color: ${theme.colors.ORANGE_1};
+  opacity: ${({ hasSeenIntro }) => (hasSeenIntro ? 1 : 0)};
+  transform: ${({ hasSeenIntro }) =>
+    hasSeenIntro ? 'translateY(0)' : 'translateY(50px)'};
+  transition: all 0.5s linear 1.25s;
 `;
 
 const Introduction = () => {
+  const { hasSeenIntro } = useIntroContext();
   const { windowWidth, windowHeight, isWindowWidthAboveOrBetweenThreshold } =
     useDeviceContext();
 
@@ -45,10 +72,14 @@ const Introduction = () => {
         calculatedWidth={windowWidth - widthDeduction}
       >
         <IntroductionContent>
-          <IntroParagraph>My name is, </IntroParagraph>
-          <Heading>Logan Garay</Heading>
-          <IntroHeading2>I am a Software Developer</IntroHeading2>
-          <IntroParagraph>
+          <IntroParagraph hasSeenIntro={hasSeenIntro}>
+            My name is,{' '}
+          </IntroParagraph>
+          <Heading hasSeenIntro={hasSeenIntro}>Logan Garay</Heading>
+          <IntroHeading2 hasSeenIntro={hasSeenIntro}>
+            I am a Software Developer
+          </IntroHeading2>
+          <IntroParagraph hasSeenIntro={hasSeenIntro} className="introduction2">
             With a passion for creating simple and elogant digital experiences.
             <br />
             Looking to make tangible impact through code to better the world
