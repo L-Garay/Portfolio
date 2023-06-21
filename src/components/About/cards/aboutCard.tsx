8ff0a2417299b53949639474723bf4ff9c43ccf1
L-Garay <garay.logan@gmail.com>
2023-06-21 16:58:16 -0600

    update the main about aboutMe card and add logic for description links

--- src/components/About/cards/aboutCard.tsx
+++ src/components/About/cards/aboutCard.tsx
@@ -153,7 +153,7 @@ const SmallParagraph = styled.p`
 
 const Link = styled.a.attrs({
   target: '_blank',
-  rel: 'noopener noreferrer',
+  rel: 'noopener noreferrer'
 })`
   color: ${theme.colors.BLUE_1};
   text-decoration: none;
@@ -191,15 +191,87 @@ const AboutCard = () => {
   const isAboveLarge = isWindowWidthAboveOrBetweenThreshold(1450);
   const aboveLarge = isAboveLarge ? isAboveLarge : false;
 
-  const profileImg = useStaticQuery(graphql`
-    {
-      file(relativePath: { eq: "squareColorPortrait.jpg" }) {
-        childImageSharp {
-          gatsbyImageData(placeholder: BLURRED)
+  const contentfulDonutChartQuery = graphql`
+    query {
+      contentfulAboutMeCards(
+        title: { eq: "You know that I know that you want to know about me" }
+      ) {
+        id
+        title
+        descriptions {
+          descriptions
+        }
+        profileImage {
+          title
+          description
+          gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
         }
       }
     }
-  `);
+  `;
+
+  const { contentfulAboutMeCards: contentfulContent } = useStaticQuery(
+    contentfulDonutChartQuery
+  );
+
+  const descriptionsArray =
+    contentfulContent.descriptions.descriptions.split('$$');
+
+  // TODO refactor the generation of these descriptions to be an abstracted function outside of this component
+  const IdahoStateLinkIndex = descriptionsArray[0].indexOf(
+    'Idaho State University'
+  );
+  const IdahoStateLinkString = descriptionsArray[0].slice(
+    IdahoStateLinkIndex,
+    IdahoStateLinkIndex + 22
+  );
+  const IdahoStateLink = (
+    <Link href="https://www.isu.edu/">{IdahoStateLinkString}</Link>
+  );
+
+  const IdahoStateDescription1 = descriptionsArray[0].slice(
+    0,
+    IdahoStateLinkIndex
+  );
+  const IdahoStateDescription2 = descriptionsArray[0].slice(
+    IdahoStateLinkIndex + 22
+  );
+  const IdahoStateDescription = () => {
+    return (
+      <p>
+        {IdahoStateDescription1}
+        {IdahoStateLink}
+        {IdahoStateDescription2}
+      </p>
+    );
+  };
+
+  const CodeworksLinkIndex = descriptionsArray[1].indexOf('Boise Codeworks');
+  const CodeworksLinkString = descriptionsArray[1].slice(
+    CodeworksLinkIndex,
+    CodeworksLinkIndex + 15
+  );
+  const CodeworksLink = (
+    <Link href="https://boisecodeworks.com/">{CodeworksLinkString}</Link>
+  );
+
+  const CodeworksDescription1 = descriptionsArray[1].slice(
+    0,
+    CodeworksLinkIndex
+  );
+  const CodeworksDescription2 = descriptionsArray[1].slice(
+    CodeworksLinkIndex + 15
+  );
+  const CodeworksDescription = () => {
+    return (
+      <p>
+        {CodeworksDescription1}
+        {CodeworksLink}
+        {CodeworksDescription2}
+      </p>
+    );
+  };
+
   return (
     <AboutCardContainer
       isAboveMobile={aboveMobile}
@@ -212,7 +284,7 @@ const AboutCard = () => {
           isAboveSmall={aboveSmall}
           isAboveLarge={aboveLarge}
         >
-          You know that I know that you want to know about me
+          {contentfulContent.title}
         </Title>
       </TitleContainer>
       <ContentContainer>
@@ -222,14 +294,10 @@ const AboutCard = () => {
             onMouseLeave={() => setIsHoveringImage(false)}
           >
             <GatsbyImage
-              image={profileImg.file.childImageSharp.gatsbyImageData}
+              image={contentfulContent.profileImage.gatsbyImageData}
               alt="black and white oval portrait of Logan Garay"
               imgStyle={{
-                width: isAboveLarge
-                  ? '200px'
-                  : isAboveSmall
-                  ? '165px'
-                  : '130px',
+                width: isAboveLarge ? '200px' : isAboveSmall ? '165px' : '130px'
               }}
               style={{
                 width: isAboveLarge
@@ -239,7 +307,7 @@ const AboutCard = () => {
                   : '130px',
                 borderRadius: '12.5px',
                 filter: isHoveringImage ? 'grayscale(0%)' : 'grayscale(100%)',
-                transition: 'filter 0.25s ease',
+                transition: 'filter 0.25s ease'
               }}
               loading="eager"
             />
@@ -255,38 +323,16 @@ const AboutCard = () => {
             isAboveSmall={aboveSmall}
             isAboveLarge={aboveLarge}
           >
-            <p>
-              These are always fun to write, am I right? Well I guess I'll tell
-              you a little about my background. I have a Bachelors of Science in
-              Psychology from{' '}
-              <Link href="https://www.isu.edu/">Idaho State University</Link>,
-              with a minor in Philosophy.
-            </p>
-            <p>
-              After I graduated in May 2019 I was left with a choice of what to
-              do with my life. My friend wanted to make video games and the
-              first step was to learn basic programming, and thanks to{' '}
-              <Link href="https://boisecodeworks.com/">Boise Codeworks</Link>{' '}
-              and 13 weeks later I was hooked. After that, well... the rest is
-              history.
-            </p>
+            <IdahoStateDescription />
+            <CodeworksDescription />
           </Description1>
           <Description2
             isAboveMobile={aboveMobile}
             isAboveSmall={aboveSmall}
             isAboveLarge={aboveLarge}
           >
-            <p>
-              When I'm not coding you can usually find me spending time with my
-              friends and two dogs, playing video games, lifting heavy weights
-              or watching some anime.
-            </p>
-            <p>
-              I have a passion for learning and am interested in all things
-              astronomy, anthropology, philosophy, history, psychology and of
-              course web development (to name a few); as I believe everyone
-              should strive to better understand the world around them.
-            </p>
+            <p>{descriptionsArray[2]}</p>
+            <p>{descriptionsArray[3]}</p>
           </Description2>
         </Row2>
         <Row3>
