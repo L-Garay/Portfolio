167e75d6ce9abc596a620812d919be25b3765d1c
L-Garay <garay.logan@gmail.com>
2023-06-27 16:32:58 -0600

    basic attempt to get live preview working

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,7 +2,25 @@ import { GatsbyBrowser } from 'gatsby';
 import wrapWithDeviceContext from './src/contexts/deviceContext';
 import wrapWithIntroContext from './src/contexts/introContext';
 import { wrapRootWithProviders } from './src/utils/wrapRootWithProviders';
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
+
+const ContentfulLivePreview = ({ element }: { element: ReactNode }) => {
+  return (
+    <ContentfulLivePreviewProvider
+      locale="en-US"
+      enableInspectorMode={false}
+      enableLiveUpdates
+      debugMode={false}
+    >
+      {element}
+    </ContentfulLivePreviewProvider>
+  );
+};
 
 export const wrapRootElement: GatsbyBrowser['wrapRootElement'] =
-  wrapRootWithProviders([wrapWithDeviceContext, wrapWithIntroContext]);
+  wrapRootWithProviders([
+    wrapWithDeviceContext,
+    wrapWithIntroContext,
+    ContentfulLivePreview
+  ]);
