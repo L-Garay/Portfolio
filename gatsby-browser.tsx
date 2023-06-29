4f181b6f74ae6af44ff46efb076ace2a94113d86
L-Garay <garay.logan@gmail.com>
2023-06-29 10:48:53 -0600

    remove root preview provider

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -3,25 +3,21 @@ import wrapWithDeviceContext from './src/contexts/deviceContext';
 import wrapWithIntroContext from './src/contexts/introContext';
 import { wrapRootWithProviders } from './src/utils/wrapRootWithProviders';
 import React, { ReactNode } from 'react';
-import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
+// import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
 import '@contentful/live-preview/style.css';
 
-const ContentfulLivePreview = ({ element }: { element: ReactNode }) => {
-  return (
-    <ContentfulLivePreviewProvider
-      locale="en-US"
-      enableInspectorMode={true}
-      enableLiveUpdates={true}
-      debugMode={true}
-    >
-      {element}
-    </ContentfulLivePreviewProvider>
-  );
-};
+// const ContentfulLivePreview = ({ element }: { element: ReactNode }) => {
+//   return (
+//     <ContentfulLivePreviewProvider
+//       locale="en-US"
+//       enableInspectorMode={true}
+//       enableLiveUpdates={true}
+//       debugMode={true}
+//     >
+//       {element}
+//     </ContentfulLivePreviewProvider>
+//   );
+// };
 
 export const wrapRootElement: GatsbyBrowser['wrapRootElement'] =
-  wrapRootWithProviders([
-    wrapWithDeviceContext,
-    wrapWithIntroContext,
-    ContentfulLivePreview
-  ]);
+  wrapRootWithProviders([wrapWithDeviceContext, wrapWithIntroContext]);
