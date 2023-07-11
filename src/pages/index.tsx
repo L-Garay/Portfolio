89b0518a186092152113c14083790d539edcd484
L-Garay <garay.logan@gmail.com>
2023-07-11 11:41:48 -0600

    account for mobile and threshold and remove hook delay option

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,30 +5,32 @@ import { useInView, defaultFallbackInView } from 'react-intersection-observer';
 import Layout from '../components/layout';
 import Introduction from '../sections/introduction';
 import GlobalStyle from '..//styles/global';
-import Journey from '../sections/journey';
 import About from '../sections/about';
 import Experience from '../sections/experience';
 import Skills from '../sections/skills';
 import { CarouselProvider } from '../contexts/carouselContext';
+import { useDeviceContext } from '../contexts/deviceContext';
 import AnimatedIntro from '../components/animatedIntro';
+import SCREEN_SIZES from '../constants/screenSizes';
 
 // Step 2: Define your component
 const IndexPage = () => {
+  const { isWindowWidthAboveOrBetweenThreshold } = useDeviceContext();
+
+  const isAboveSmall = isWindowWidthAboveOrBetweenThreshold(SCREEN_SIZES.SMALL);
+
   defaultFallbackInView(true);
 
   const { ref: skillsRef, inView: skillsInView } = useInView({
-    threshold: 0.5,
-    delay: 250,
+    threshold: isAboveSmall ? 0.5 : 0.35, // may need to tweak this
     triggerOnce: true
   });
   const { ref: experienceRef, inView: experienceInView } = useInView({
     threshold: 0.5,
-    delay: 250,
     triggerOnce: true
   });
   const { ref: aboutRef, inView: aboutInView } = useInView({
-    threshold: 0.5,
-    delay: 250,
+    threshold: isAboveSmall ? 0.5 : 0.15, // may need to tweak this
     triggerOnce: true
   });
 
@@ -40,7 +42,6 @@ const IndexPage = () => {
         <Introduction />
         <Skills ref={skillsRef} inView={skillsInView} />
         <Experience ref={experienceRef} inView={experienceInView} />
-        {/* <Journey /> */}
         <CarouselProvider>
           <About ref={aboutRef} inView={aboutInView} />
         </CarouselProvider>
